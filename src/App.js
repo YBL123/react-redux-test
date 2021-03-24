1da00429c757ebe1f483f32061170c5ce0a42a38
Yarden Lawson <yardenlawson@gmail.com>
2021-03-24 14:54:33 +0000

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function App() {
   const [data, setData] = useState([]);
   const postWordCount = [];
-  const allWords = []
+  const allWords = [];
 
   useEffect(async () => {
     const result = await axios('https://jsonplaceholder.typicode.com/posts');
@@ -16,44 +16,76 @@ function App() {
   console.log('boop', data);
 
   for (let i = 0; i < data.length; i++) {
-  //   // console.log('here', data[i]);
-  //   const wordCount = data[i].title.length + data[i].body.length;
+    //   // console.log('here', data[i]);
+    //   const wordCount = data[i].title.length + data[i].body.length;
 
-  //   console.log(wordCount);
-  //   postWordCount.push(wordCount);
-  //   console.log(postWordCount)
-    allWords.push(data[i].title + data[i].body)
-    console.log('allWords', allWords)
-    
+    //   console.log(wordCount);
+    //   postWordCount.push(wordCount);
+    //   console.log(postWordCount)
+    allWords.push(data[i].title + data[i].body);
+    // console.log('allWords', allWords); /// HEEERERER
   }
 
   // const totalWordCount = postWordCount.reduce((acc, curr) => acc + curr);
   // console.log(totalWordCount);
 
+  // function calcWordFreq(s) {
+  //   // Normalize
+  //   s = s.toLowerCase();
+  //   // Strip quotes and brackets
+  //   s = s.replace(/["“”(\[{}\])]|\B['‘]([^'’]+)['’]/g, '$1');
+  //   // Strip dashes and ellipses
+  //   s = s.replace(/[‒–—―…]|--|\.\.\./g, ' ');
+  //   // Strip punctuation marks
+  //   s = s.replace(/[!?;:.,]\B/g, '');
+  //   return s.match(/\S+/g).reduce(function(oFreq, sWord) {
+  //     if (oFreq.hasOwnProperty(sWord)) ++oFreq[sWord];
+  //     else oFreq[sWord] = 1;
+  //     return oFreq;
+  //   }, {});
+  // }
 
-  function calcWordFreq(s) {
-    // Normalize
-    s = s.toLowerCase();
-    // Strip quotes and brackets
-    s = s.replace(/["“”(\[{}\])]|\B['‘]([^'’]+)['’]/g, '$1');
-    // Strip dashes and ellipses
-    s = s.replace(/[‒–—―…]|--|\.\.\./g, ' ');
-    // Strip punctuation marks
-    s = s.replace(/[!?;:.,]\B/g, '');
-    return s.match(/\S+/g).reduce(function(oFreq, sWord) {
-      if (oFreq.hasOwnProperty(sWord)) ++oFreq[sWord];
-      else oFreq[sWord] = 1;
-      return oFreq;
+  // console.log('boopdidoop', allWords.map(word => word.split(' ') ))
+
+  const x = allWords.map((word) => word.split(' '));
+
+  let z = x.flat();
+
+  console.log('yoooo', z);
+
+  for (let i = 0; i < z.length; i++) {
+    z.reduce(function (acc, curr) {
+      if (typeof acc[curr] == 'undefined') {
+        acc[curr] = 1;
+      } else {
+        acc[curr] += 1;
+      }
+
+      return acc
+
+      // const y = acc.filter( (item, index) => {
+      //   if (index <= 2 || Object.values(item)[0] === Object.values(acc[0])[0]) {
+      //     return item
+      //   }
+      // })
+
+      // console.log('is this working', y)
     }, {});
   }
+  // console.log('hereeererere', acc);
+  // const sortedValues = Object.values(acc)
+  //   .flatMap(Object.entries)
+  //   .sort(([, a], [, b]) => b - a);
+  // const fiveHighest = sortedValues.slice(0, 5);
 
-  console.log('boopdidoop', allWords.map(word => calcWordFreq(word) ))
+
 
   return (
     <>
     {/* <h1>Toal Word Count: {totalWordCount}</h1> */}
     </>
-  );
+  )
+
 }
 
 export default App;
